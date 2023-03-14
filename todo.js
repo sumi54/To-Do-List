ba9ff5f44b97fa5bc20da2c0ead153afecad1594
sumi54 <sumeyyekarakullukcu34@gmail.com>
2023-03-14 10:53:15 +0300

    filtreleme ve tümünü silme

--- todo.js
+++ todo.js
@@ -10,17 +10,41 @@ eventListeners();
 
 function eventListeners() {
     form.addEventListener("submit", addTodo);
-    document.addEventListener("DOMContentLoaded",loadAllTodosToUI);
-    secondCardBody.addEventListener("click",deleteTodo);
-    filter.addEventListener("keyup",filterTodos);
+    document.addEventListener("DOMContentLoaded", loadAllTodosToUI);
+    secondCardBody.addEventListener("click", deleteTodo);
+    filter.addEventListener("keyup", filterTodos);
+    clearbutton.addEventListener("click",clearAll);
 }
-function deleteTodo(e){
-    if(e.target.className === "fa fa-remove"){
-        e.target.parentElement.parentElement.remove();
-        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
-        showAlert("success","To Do başarıyla silindi.");
+function clearAll(e){
+    if(confirm("Tümünü silmek istediğinize emin misiniz?")){
+        //todolist.innerHTML=""; //yavaş çözüm
+        while(todolist.firstElementChild != null){
+            todolist.removeChild(todolist.firstElementChild);
+        }
+        localStorage.removeItem("todos");
     }
 }
+function filterTodos(e) {
+    let filterValue = e.target.value.toLowerCase();
+    let listItems = document.querySelectorAll(".list-group-item");
+    listItems.forEach(function (listItem) {
+        let text = listItem.textContent.toLowerCase();
+        if (text.indexOf(filterValue) === -1) {
+            listItem.setAttribute("style", "display:none !important");
+        } else {
+            listItem.setAttribute("style", "display:block");
+        }
+    });
+}
+
+function deleteTodo(e) {
+    if (e.target.className === "fa fa-remove") {
+        e.target.parentElement.parentElement.remove();
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
+        showAlert("success", "To Do başarıyla silindi.");
+    }
+}
+
 function addTodo(e) {
     let newTodo = todoinput.value.trim();
 
@@ -35,14 +59,15 @@ function addTodo(e) {
 
     e.preventDefault();
 }
-function deleteTodoFromStorage(deletetodo){
-    let todos=getTodosFromStorage();
-    todos.forEach(function(todo,index){
-        if(todo===deletetodo){
-            todos.splice(index,1); //arrayden değer silme
+
+function deleteTodoFromStorage(deletetodo) {
+    let todos = getTodosFromStorage();
+    todos.forEach(function (todo, index) {
+        if (todo === deletetodo) {
+            todos.splice(index, 1); //arrayden değer silme
         }
     });
-    localStorage.setItem("todos",JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 //alert oluşturma
 function showAlert(type, message) {
@@ -51,31 +76,33 @@ function showAlert(type, message) {
     alert.textContent = message;
     firstCardBody.appendChild(alert);
     setTimeout(function () {
-            alert.remove();      
-    },1500);
+        alert.remove();
+    }, 1500);
 }
-function getTodosFromStorage(){//storageden todoları alma
+
+function getTodosFromStorage() { //storageden todoları alma
     let todos;
-    if(localStorage.getItem("todos")===null){
-        todos=[];
-    }
-    else{
-        todos=JSON.parse(localStorage.getItem("todos"));
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
     }
     return todos;
 }
-function addTodoStorage(newTodo){
-    let todos =getTodosFromStorage();
+
+function addTodoStorage(newTodo) {
+    let todos = getTodosFromStorage();
     todos.push(newTodo);
-    localStorage.setItem("todos",JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function loadAllTodosToUI(){
-    let todos=getTodosFromStorage();
-    todos.forEach(function(todo){
+function loadAllTodosToUI() {
+    let todos = getTodosFromStorage();
+    todos.forEach(function (todo) {
         addTodoToUI(todo);
     })
 }
+
 function addTodoToUI(newTodo) {
     // < li class = "list-group-item d-flex justify-content-between" >
     //     Todo 1 < ------------------------------------------------------------>>TEXT NODE
